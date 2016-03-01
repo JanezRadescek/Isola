8741e29d9a252b63db5a35bc1c17a439903dec3d
JanezRadescek <janez.radescek@gmail.com>
2016-03-01 16:43:19 +0100

    Mal popravkov

--- IsolaCP.py
+++ IsolaCP.py
@@ -2,12 +2,13 @@ import tkinter
 import random
 
 #isue  shranjevanje poteze za premik in uničevanje posebej ne vem če vredu ??
-PRAZNO = None
+VELJAVNO = None
 IGRALEC_1 = 1
 IGRALEC_2 = 2
 UNICENO = 0
-
+ZACETNI_IGRALEC = IGRALEC_1
 NEODLOCENO = None
+
 
 def nasprotnik(igralec):
     #določi nasprotnika
@@ -16,18 +17,16 @@ def nasprotnik(igralec):
     else:
         return IGRALEC_1
 
+
 class Igra():
     def __init__(self):
-
-        self.polje = list([0]*7 for i in range(7))
+        self.polje = list([VELJAVNO]*7 for _ in range(7))
         self.polje[0][3] = 1
         self.polje[6][3] = 2
-        self.na_potezi = IGRALEC_1
+        self.na_potezi = ZACETNI_IGRALEC
         self.zgodovina = []
-        self.pozicija_1 = (0,3)
-        self.pozicija_2 = (6,3)
-
-
+        self.pozicija_1 = (0, 3)
+        self.pozicija_2 = (6, 3)
 
     def shrani_pozicijo(self):
         p = [self.polje[i][:] for i in range(7)]
@@ -37,30 +36,29 @@ class Igra():
         (self.polje, self.na_potezi) = self.zgodovina.pop()
 
     def je_veljavna(self, i, j):
-        return (self.polje[i][j] is None)
+        return (self.polje[i][j] == VELJAVNO)
 
     def je_konec(self):
         #treba še delat
         pass
 
     def pozicija_na_potezi(self):
-        #vrne par koordinate igralca na potezi
+        # vrne par koordinate igralca na potezi
         if self.na_potezi == IGRALEC_1:
             return self.pozicija_1
         else:
             return self.pozicija_2
 
-
     def veljavne_poteze_premik(self):
-        #preveri vsa polja okoli igralca na potezi in vrne seznam polj na katere se lahko premakne
+        # preveri vsa polja okoli igralca na potezi in vrne seznam polj na katere se lahko premakne
         poteze = []
-        (a,b) = self.pozicija_na_potezi()
+        (a, b) = self.pozicija_na_potezi()
         for i in range(3):
             for j in range(3):
-                c = a-1+i
-                d = b-1+j
-                if c>=0 and c<=6 and d>=0 and d<=6 and self.je_veljavna(c,d):
-                    poteze.append((i,j))
+                c = a - 1 + i
+                d = b - 1 + j
+                if c >= 0 and c <= 6 and d >= 0 and d <= 6 and self.je_veljavna(c, d):
+                    poteze.append((i, j))
         return poteze
 
     def veljavne_poteze_unici(self):
@@ -69,23 +67,37 @@ class Igra():
         for i in range(7):
             for j in range(7):
                 if self.je_veljavna(i, j):
-                    poteze.append((i,j))
+                    poteze.append((i, j))
         return poteze
 
-
-
     def premik(self, i, j):
-        #premaknemo se na veljavno polje in staro polje naredimo spet veljavno
-        if (i,j) in self.veljavne_poteze_premik():
+        #premaknemo se na veljavno polje, staro polje naredimo spet veljavno, zapišemo pozicijo igralca
+        if (i, j) in self.veljavne_poteze_premik():
             self.shrani_pozicijo()
             self.polje[i][j] = self.na_potezi
-            (c,d) = self.pozicija_na_potezi()
-            self.polje[c][d] = None
+            (c, d) = self.pozicija_na_potezi()
+            self.polje[c][d] = VELJAVNO
+            if self.na_potezi == IGRALEC_1:
+                self.pozicija_1 = (i, j)
+            else:
+                self.pozicija_2 = (i, j)
 
     def unici(self, i, j):
         #uniči veljavno polje
-        if (i,j) in self.veljavne_poteze_unici():
+        if (i, j) in self.veljavne_poteze_unici():
             self.shrani_pozicijo()
             self.polje[i][j] = UNICENO
+
+    def zmagovalec(self):
+        pass
+
+
+
+
+
+
+
+
+
 
 
