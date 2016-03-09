63041c7d3fc40dd91711caabe4027c0610226f7b
KappaCancer <janez.radescek@hotmail.com>
2016-03-09 14:04:14 +0100

    FAKS 2.1
    zgodovina ne dela. ni še za ogled

--- IsolaCP.py
+++ IsolaCP.py
@@ -2,10 +2,13 @@ import tkinter
 import random
 from itertools import product
 import winsound
+import logging
+import argparse
+import threading
 
 
-
-
+VELIKOST_POLJA = 70
+GLOBINA = 3
 VELJAVNO = None
 IGRALEC_1 = 1
 IGRALEC_2 = 2
@@ -20,6 +23,15 @@ NAPIS_IGRALEC2_PREMIK = "Na potezi je ZELENI igralec. Cas je da se premaknes."
 NAPIS_IGRALEC1_UNICENJE = "Na potezi je MODRI igralec. Cas je da unicis polje."
 NAPIS_IGRALEC2_UNICENJE = "Na potezi je ZELENI igralec. Cas je da unicis polje."
 
+NAVODILA = """just kiding \n hahahah \n \n
+Na polju 7*7 igralca postavita svojo figurico
+na sredino nasprotnih robov. Igralca si izmenjujeta
+poteze. V vsaki potezi se igralec najprej premakne
+za eno polje(kot kralj pri šahu), nato odstrani
+eno polje. Na polja, ki so že bila odstranjena
+se ni dovoljeno premakniti. Cilj igre je odstraniti
+ploščice tako, da se nasprotnik ne more več premakniti."""
+
 
 def nasprotnik(igralec):
     #določi nasprotnika
@@ -30,13 +42,16 @@ def nasprotnik(igralec):
         return IGRALEC_1
 
 
+######################################################    class Igra      #############################
+
+
 class Igra():
     '''napisana je logika igre '''
 
     def __init__(self):
         self.polje = list([VELJAVNO]*7 for _ in range(7)) #mreza za shranjevanje veljavnih polj
-        self.polje[0][3] = 1
-        self.polje[6][3] = 2
+        self.polje[0][3] = IGRALEC_1
+        self.polje[6][3] = IGRALEC_2
         self.na_potezi = ZACETNI_IGRALEC
         self.zgodovina = []
         self.pozicija_1 = (0, 3)
@@ -46,6 +61,23 @@ class Igra():
     def shrani_pozicijo(self):
         p = [self.polje[i][:] for i in range(7)]
         self.zgodovina.append((p, self.na_potezi))
+        print(self.na_potezi)
+
+    def kopija(self):
+        """Vrni kopijo te igre, brez zgodovine."""
+        # Kopijo igre naredimo, ko poženemo na njej algoritem.
+        # Če bi algoritem poganjali kar na glavni igri, ki jo
+        # uporablja GUI, potem bi GUI mislil, da se menja stanje
+        # igre (kdo je na potezi, kdo je zmagal) medtem, ko bi
+        # algoritem vlekel poteze
+        k = Igra()
+        k.polje = [self.polje[i][:] for i in range(7)]
+        k.na_potezi = self.na_potezi
+        k.pozicija_1 = self.pozicija_1
+        k.pozicija_2 = self.pozicija_2
+        k.del_poteze = self.del_poteze
+        k.zgodovina = self.zgodovina
+        return k
 
     def razveljavi(self):
         (self.polje, self.na_potezi) = self.zgodovina.pop()
@@ -67,11 +99,21 @@ class Igra():
         else:
             return self.pozicija_2
 
-    def veljavne_poteze_premik(self):
+    def pozicija_nasprotnik(self):
+        if self.na_potezi == IGRALEC_2:
+            return self.pozicija_1
+        else:
+            return self.pozicija_2
+
+
+    def veljavne_poteze_premik(self, nasprotnik = False ):
         ''' preveri vsa polja okoli igralca na potezi in vrne seznam polj na katere se lahko premakne'''
 
         poteze = []
-        (a, b) = self.pozicija_na_potezi()
+        if not nasprotnik:
+            (a, b) = self.pozicija_na_potezi()
+        else:
+            (a, b) = self.pozicija_nasprotnik()
         for i in range(3):
             for j in range(3):
                 c = a - 1 + i
@@ -126,7 +168,7 @@ class Igra():
             return self.na_potezi
 
 
-#################################################### človek
+####################################################       človek        #####################
 
 
 class Clovek():
@@ -146,13 +188,211 @@ class Clovek():
         self.gui.povleci_potezo(i, j)
 
 
+#########################################################     racunalnik    #############################
 
-####################################################### gui
+class Racunalnik():
+    '''Igralec, ki ga upravlja racunalnik'''
+
+    def __init__(self, gui, algoritem):
+        self.gui = gui
+        self.algoritem = algoritem
+        self.mislec = None
+
+    def igraj(self):
+        self.mislec = threading.Thread(
+            target=lambda: self.algoritem.izracunaj_potezo(self.gui.igra.kopija()))
+
+        self.mislec.start()
+
+        self.gui.plosca.after(100, self.preveri_potezo)
+
+    def preveri_potezo(self):
+        """Vsakih 100ms preveri, ali je algoritem že izračunal potezo."""
+        if self.algoritem.poteza is not None:
+            # Algoritem je našel potezo, povleci jo, če ni bilo prekinitve
+            self.gui.povleci_potezo(self.algoritem.poteza)
+            # Vzporedno vlakno ni več aktivno, zato ga "pozabimo"
+            self.mislec = None
+        else:
+            # Algoritem še ni našel poteze, preveri še enkrat čez 100ms
+            self.gui.plosca.after(100, self.preveri_potezo)
+
+    def prekini(self):
+        # To metodo kliče GUI, če je treba prekiniti razmišljanje.
+        if self.mislec:
+            logging.debug ("Prekinjamo {0}".format(self.mislec))
+            # Algoritmu sporočimo, da mora nehati z razmišljanjem
+            self.algoritem.prekini()
+            # Počakamo, da se vlakno ustavi
+            self.mislec.join()
+            self.mislec = None
+
+    def klik(self):
+        pass
+
+
+#################################################       Algoritem minimax       ###################
+
+class Minimax():
+    # Algoritem minimax predstavimo z objektom, ki hrani stanje igre in
+    # algoritma, nima pa dostopa do GUI (ker ga ne sme uporabljati, saj deluje
+    # v drugem vlaknu kot tkinter).
+    def __init__(self, globina = GLOBINA):
+        self.globina = globina  # do katere globine iščemo?
+        self.prekinitev = False # ali moramo končati?
+        self.igra_kopija = None # objekt, ki opisuje igro (ga dobimo kasneje)
+        self.jaz = None  # katerega igralca igramo (podatek dobimo kasneje)
+        self.poteza = None # sem napišemo potezo, ko jo najdemo
+
+    def prekini(self):
+        """Metoda, ki jo pokliče GUI, če je treba nehati razmišljati, ker
+           je uporabnik zaprl okno ali izbral novo igro."""
+        self.prekinitev = True
+
+
+    def izracunaj_potezo(self, igra):
+        """Izračunaj potezo za trenutno stanje dane igre."""
+        # To metodo pokličemo iz vzporednega vlakna
+        self.igra_kopija = igra
+        self.prekinitev = False # Glavno vlakno bo to nastvilo na True, če moramo nehati
+        self.jaz = self.igra_kopija.na_potezi
+        self.poteza = None # Sem napišemo potezo, ko jo najdemo
+        # Poženemo minimax
+        (poteza, vrednost) = self.minimax(self.globina, True)
+        self.jaz = None
+        self.igra_kopija = None
+        if not self.prekinitev:
+            # Potezo izvedemo v primeru, da nismo bili prekinjeni
+            logging.debug("minimax: poteza {0}, vrednost {1}".format(poteza, vrednost))
+            self.poteza = poteza
+
+    ZMAGA = 100000 # Mora biti vsaj 10^5
+    NESKONCNO = ZMAGA + 1 # Več kot zmaga
+
+
+
+    def vrednost_pozicije_premik(self, i, j):
+        return 100
+
+    def vrednost_pozicije_unici(self, i, j):
+        return 100
+
+
+
+    def minimax(self, globina, maksimiziramo):
+        if self.prekinitev:
+             # Sporočili so nam, da moramo prekiniti
+             logging.debug ("Minimax prekinja, globina = {0}".format(globina))
+             return (None, 0)
+        if self.igra_kopija.del_poteze == PREMIK:
+            return self.premik_minimax(globina, maksimiziramo)
+        else:
+            return self.unici_minimax(globina, maksimiziramo)
+
+    def premik_minimax(self, globina, maksimiziramo):
+        (i, j) = self.igra_kopija.pozicija_na_potezi()
+        if globina == 0:
+            return ((i,j), self.vrednost_pozicije_premik(i, j))
+        else:
+            if maksimiziramo:
+                # Maksimiziramo
+                najboljsa_poteza = None
+                vrednost_najboljse = -Minimax.NESKONCNO
+                for (i, j) in self.igra_kopija.veljavne_poteze_premik():
+                    self.igra_kopija.naredi_pravo_potezo(i, j)
+                    (p,vrednost) = self.minimax(globina-1, not maksimiziramo)
+                    self.igra_kopija.razveljavi()
+                    if vrednost > vrednost_najboljse:
+                        vrednost_najboljse = vrednost
+                        najboljsa_poteza = p
+                return najboljsa_poteza
+            else:
+                najboljsa_poteza = None
+                vrednost_najboljse = Minimax.NESKONCNO
+                for (i, j) in self.igra_kopija.veljavne_poteze_premik():
+                    self.igra_kopija.naredi_pravo_potezo(i, j)
+                    (p,vrednost) = self.minimax(globina-1, not maksimiziramo)
+                    self.igra_kopija.razveljavi()
+                    if vrednost < vrednost_najboljse:
+                        vrednost_najboljse = vrednost
+                        najboljsa_poteza = p
+                return najboljsa_poteza
+
+
+
+
+    def unici_minimax(self, globina, maksimiziramo):
+        (i, j) = self.igra_kopija.pozicija_na_potezi()
+        if globina == 0:
+            return ((i,j), self.vrednost_pozicije_unici(i, j))
+        else:
+            if maksimiziramo:
+                # Maksimiziramo
+                najboljsa_poteza = None
+                vrednost_najboljse = -Minimax.NESKONCNO
+                for (i, j) in self.igra_kopija.veljavne_poteze_unici():
+                    self.igra_kopija.naredi_pravo_potezo(i, j)
+                    (p,vrednost) = self.minimax(globina-1, not maksimiziramo)
+                    self.igra_kopija.razveljavi()
+                    if vrednost > vrednost_najboljse:
+                        vrednost_najboljse = vrednost
+                        najboljsa_poteza = p
+                return najboljsa_poteza
+            else:
+                najboljsa_poteza = None
+                vrednost_najboljse = Minimax.NESKONCNO
+                for (i, j) in self.igra_kopija.veljavne_poteze_unici():
+                    self.igra_kopija.naredi_pravo_potezo(i, j)
+                    (p,vrednost) = self.minimax(globina-1, not maksimiziramo)
+                    self.igra_kopija.razveljavi()
+                    if vrednost < vrednost_najboljse:
+                        vrednost_najboljse = vrednost
+                        najboljsa_poteza = p
+                return najboljsa_poteza
+
+    # def minimax(self, globina, maksimiziramo):
+    #     if self.prekinitev:
+    #         # Sporočili so nam, da moramo prekiniti
+    #         logging.debug ("Minimax prekinja, globina = {0}".format(globina))
+    #         return (None, 0)
+    #     if globina == 0:
+    #             return (None, self.vrednost_pozicije())
+    #     else:
+    #         # Naredimo eno stopnjo minimax
+    #         if maksimiziramo:
+    #             # Maksimiziramo
+    #             najboljsa_poteza = None
+    #             vrednost_najboljse = -Minimax.NESKONCNO
+    #             for p in self.igra_kopija.veljavne_poteze():
+    #                 self.igra_kopija.povleci_potezo(p)
+    #                 vrednost = self.minimax(globina-1, not maksimiziramo)[1]
+    #                 self.igra_kopija.razveljavi()
+    #                 if vrednost > vrednost_najboljse:
+    #                     vrednost_najboljse = vrednost
+    #                     najboljsa_poteza = p
+    #         else:
+    #             # Minimiziramo
+    #             najboljsa_poteza = None
+    #             vrednost_najboljse = Minimax.NESKONCNO
+    #             for p in self.igra_kopija.veljavne_poteze():
+    #                 self.igra_kopija.povleci_potezo(p)
+    #                 vrednost = self.minimax(globina-1, not maksimiziramo)[1]
+    #                 self.igra_kopija.razveljavi()
+    #                 if vrednost < vrednost_najboljse:
+    #                     vrednost_najboljse = vrednost
+    #                     najboljsa_poteza = p
+    #         return (najboljsa_poteza, vrednost_najboljse)
+
+
+
+
+
+#########################################################     class Gui     ################################
 
 class Gui():
     '''graficni vmesnik'''
 
-    def __init__(self, master, velikost):
+    def __init__(self, master, velikost, globina = GLOBINA):
         self.napis = tkinter.StringVar()
         self.napis.set(NAPIS_IGRALEC1_PREMIK)
         #self.napis = tkinter.StringVar(master, value=NAPIS_IGRALEC1)
@@ -168,12 +408,14 @@ class Gui():
         self.kvadratki = self.narisi_kvadratke()
 
         self.izbira_igralcev()
+        self.globina = globina
 
 
 
     def izbira_igralcev(self):
         self.igralec_1 = Clovek(self)
-        self.igralec_2 = Clovek(self)
+        #self.igralec_2 = Clovek(self)
+        self.igralec_2 = Racunalnik(self, Minimax(GLOBINA))
         self.zacni_igro()
 
     def zacni_igro(self):
@@ -188,6 +430,7 @@ class Gui():
 
     def koncaj_igro(self):
         '''Nastavi stanje igre na konec igre.'''
+
         print ("KONEC!")
 
     def povleci_potezo(self, i, j):
@@ -221,16 +464,11 @@ class Gui():
         if self.igra.je_konec():
                 self.koncaj_igro()
         else:
-            if self.igra.del_poteze == UNICENJE:
-                if self.igra.na_potezi == IGRALEC_1:
-                    self.igralec_1.igraj()
-                else:
-                    self.igralec_2.igraj()
+            if self.igra.na_potezi == IGRALEC_1:
+                self.igralec_1.igraj()
             else:
-                if self.igra.na_potezi == IGRALEC_1:
-                    self.igralec_2.igraj()
-                else:
-                    self.igralec_1.igraj()
+                self.igralec_2.igraj()
+
 
 
 
@@ -291,14 +529,15 @@ class Gui():
         else:
             self.napis.set(napis)
 
-
+#########################################################    class Meni    ##################################
 
 
 class Meni():
     '''meni'''
 
-    def __init__(self, master):
+    def __init__(self, master, velikost_polja):
         self.master = master
+        self.velikost_polja = velikost_polja
 
         winsound.PlaySound("files\\two.wav", winsound.SND_ASYNC|winsound.SND_LOOP)
 
@@ -330,21 +569,22 @@ class Meni():
 
 
     def play(self, event = None):
-        self.aplication2 = Gui(root, 70)
+        self.aplication2 = Gui(root, self.velikost_polja, GLOBINA)
 
     def options(self):
         print("options")
         pass
 
     def help(self):
-
-        napis_help = "Vse ob svojem času"
+        napis_help = "NAVODILA"
+        navodila = NAVODILA
 
         self.aplication2.spremeni_napis(napis_help)
 
-        self.velikost_plosce = 7 * 50
+        self.velikost_plosce = 7 * self.velikost_polja
         self.plosca = tkinter.Canvas(self.master, width=self.velikost_plosce, height=self.velikost_plosce)
         self.plosca.grid(row=1, column=0, columnspan = 4)
+        self.plosca.create_text(self.velikost_plosce/2, self.velikost_polja*5, text = navodila)
 
         print("help")
         pass
@@ -360,7 +600,7 @@ if __name__ == "__main__":
     # Naredimo objekt razreda Gui in ga spravimo v spremenljivko,
     # sicer bo Python mislil, da je objekt neuporabljen in ga bo pobrisal
     # iz pomnilnika.
-    aplication1 = Meni(root)
+    aplication1 = Meni(root, VELIKOST_POLJA)
     # Kontrolo prepustimo glavnemu oknu. Funkcija mainloop neha
     # delovati, ko okno zapremo.
     root.mainloop()
